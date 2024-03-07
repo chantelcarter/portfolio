0974d83e90d611fc2621bad4ba2216c9da7e54a2
chantelcarter <chantel.alexia@gmail.com>
2024-03-06 21:34:41 -0800

    updated background and home intro

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,10 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-      <div>Hi, I'm Chantel</div>
+      <div className='home-intro'>
+        <div className='hi-im-chantel'>Hi, I'm Chantel.</div>
+        <p>A full-stack web developer with experience in React JS, Ruby on Rails, Jest, and RSpect testing. </p>
+      </div>
     </>
   )
 }
