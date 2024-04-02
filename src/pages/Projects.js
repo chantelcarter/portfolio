8d89748ee232f3366574aef3dd658c1cd3d35e06
Chantel Carter <chantel.alexia@gmail.com>
2024-04-02 16:39:22 -0700

    projects styling

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -6,46 +6,46 @@ import LaunchIcon from '@mui/icons-material/Launch'
 
 const Projects = () => {
   return (
-    <div className='project'>
+    <div>
       <h2>Projects</h2>
-      <br />
-    {projects.map((projects) => (
-      <div>
-        <h4>{projects.name}</h4>
-        <img src={projects.image} alt="test project" className='project-image' width="350"/>
-        <p className='project__description'>{projects.description}</p>
-        {projects.stack && (
-          <ul className='project__stack'>
-            {projects.stack.map((item) => (
-              <li key={uniqid()} className='project__stack-item'>
-                {item}
-              </li>
-            ))}
-          </ul>
-        )}
+      <div className='project'>
+      {projects.map((projects) => (
+        <div>
+          <h4>{projects.name}</h4>
+          <img src={projects.image} alt="test project" className='project-image' width="350"/>
+          <p className='project__description'>{projects.description}</p>
+          {projects.stack && (
+            <ul className='project__stack'>
+              {projects.stack.map((item) => (
+                <li key={uniqid()} className='project__stack-item'>
+                  {item}
+                </li>
+              ))}
+            </ul>
+          )}
 
-        {projects.sourceCode && (
-          <a
-            href={projects.sourceCode}
-            aria-label='source code'
-            className='link link--icon'
-          >
-            <GitHubIcon />
-          </a>
-        )}
+          {projects.sourceCode && (
+            <a
+              href={projects.sourceCode}
+              aria-label='source code'
+              className='link link--icon'
+            >
+              <GitHubIcon />
+            </a>
+          )}
 
-        {projects.livePreview && (
-          <a
-            href={projects.livePreview}
-            aria-label='live preview'
-            className='link link--icon'
-          >
-            <LaunchIcon />
-          </a>
-        )}
-      </div>      
-    ))}
-    
+          {projects.livePreview && (
+            <a
+              href={projects.livePreview}
+              aria-label='live preview'
+              className='link link--icon'
+            >
+              <LaunchIcon />
+            </a>
+          )}
+        </div>      
+      ))}
+    </div>
   </div>
   )
 }
